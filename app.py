b1670e9160089fd05c227a30826f3ec0b0404432
Nikita Molchanov <nikita@PARCNEW>
2025-09-10 16:40:25 +0300

    Добавлены новые модели

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
 
-from common.bot_cmds_list import private
+#from common.bot_cmds_list import private
 
 #ALLOWED_UPDATES = ['message, edited_message', 'callback_query']
 
@@ -37,9 +37,7 @@ dp.include_router(admin_router)
 
 async def on_startup(bot):
 
-    run_param = False
-    if run_param:
-        await drop_db()
+    await drop_db()
 
     await create_db()
 
@@ -55,8 +53,8 @@ async def main():
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
     
     await bot.delete_webhook(drop_pending_updates=True)
-    #await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
-    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+    #await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
