04977273a2ce65f22bbb0b07c019d43598efaa96
Nikita Molchanov <nikita@PARCNEW>
2025-09-24 15:25:13 +0300

    Версия 1.18, Кнопка купить добавляет товар в корзину, но пока не выводит

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -22,14 +22,27 @@ async def start_cmd(message: types.Message, session: AsyncSession):
     media, reply_markup = await get_menu_content(session, level=0, menu_name="main")
 
     await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
-    
+
+
+async def add_to_cart(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    user = callback.from_user
+    await orm_add_user(
+        session,
+        user_id=user.id,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone=None,
+    )
+    await orm_add_to_cart(session, user_id=user.id, product_id=callback_data.product_id)
+    await callback.answer("Товар добавлен в корзину.")
+        
     
 @user_private_router.callback_query(MenuCallBack.filter())
 async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
 
-    """if callback_data.menu_name == "add_to_cart":
+    if callback_data.menu_name == "add_to_cart":
         await add_to_cart(callback, callback_data, session)
-        return"""
+        return
 
     media, reply_markup = await get_menu_content(
         session,
